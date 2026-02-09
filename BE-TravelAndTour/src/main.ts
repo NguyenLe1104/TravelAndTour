4ae2f104d1b2b4e442f3c93565adc7a66c169258
NguyenLe1104 <levannguyen2k44@gmail.com>
2026-02-09 22:01:58 +0700

    fix docker BE not running

--- BE-TravelAndTour/src/main.ts
+++ BE-TravelAndTour/src/main.ts
@@ -7,6 +7,11 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
